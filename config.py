1098eeb03374d636bf60df950e29ba036eae6dcb
DineshBabu1201 <dineshbabu12012004@gmail.com>
2024-07-14 18:51:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6987286137:AAETx4VUvxg2INIjeO2rI_d8pDTu08MrlK0")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "20911790"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "7da608ea8b4f2b1c15a4e321cbca13c5")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://dineshbabum12c:D0DxD9dSz5t5MixH@diniclick.hk2msqv.mongodb.net/?retryWrites=true&w=majority&appName=DiniClick")
